00414a481d8fd64f080b40bcb4bafe71a4dd7958
Krutik Bajariya <krutikbajariya123@gmail.com>
2025-05-03 23:29:38 -0400

    #OPENTOWORK

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ Version:  1.0
 Author:		Nazar Miller (millerDigitalDesign)
 Portfolio:  https://themeforest.net/user/millerdigitaldesign/portfolio?ref=MillerDigitalDesign
 
-p.s. I am available for Freelance hire (UI design, web development). mail: miller.themes@gmail.com
+p.s. #OPENTOWORK (UI design, web development). mail: miller.themes@gmail.com
 
 ------------------------------------------- */
 $(function() {
